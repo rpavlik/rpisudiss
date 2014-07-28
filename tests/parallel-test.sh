678976623f9c4f8c213ca77db6898d712caa622e
Ryan Pavlik <rpavlik@iastate.edu>
2014-07-28 13:36:37 -0500

    Clean before running either of the shell-driven test drivers

--- tests/parallel-test.sh
+++ tests/parallel-test.sh
@@ -6,6 +6,8 @@ TESTDIR="$(cd $(dirname $0) && pwd)"
 
 (
 cd $TESTDIR
+rm -rf results
+rm -f report/*.html
 
 find . -name "test-*.sh" -print0 | parallel -0 sh
 )
--- tests/run-all-tests.sh
+++ tests/run-all-tests.sh
@@ -4,6 +4,8 @@ TESTDIR="$(cd $(dirname $0) && pwd)"
 
 (
 cd $TESTDIR
+rm -rf results
+rm -f report/*.html
 
 find . -name "test-*.sh" -print0 | xargs -n1 -0 sh
 )
