a524b8f74bc5e3d8378f9536c7628204e3a55fa3
Ryan Pavlik <rpavlik@iastate.edu>
2014-07-28 14:18:20 -0500

    re-org and document testutils.sh

--- tests/testutils.sh
+++ tests/testutils.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
+###
+# BEGIN SETUP CODE
+
+# Load sh2ju innards
+. $TESTDIR/sh2ju.sh
+
+# Prepare temporary directory
+scratch=$(mktemp -d -t tmp.testdissertation.XXXXXXXXXX)
+# Function to tear down test: copy pdf and delete temp directory.
+finish() {
+  if [ -f "$TARGETNAME.pdf" ]; then
+    cp "$TARGETNAME.pdf" "${RESULTSDIR}/${suite}.pdf"
+  fi
+  rm -rf "$scratch"
+}
+trap finish EXIT
+
+# Set up variables for use by either the build or test scripts.
+export DIR="$scratch"
+export BASICLOG="$scratch/basiclog.txt"
+export LOG="$scratch/$DOCNAME.log"
+export TARGETNAME="$scratch/$DOCNAME"
+export RESULTSDIR="${juDIR}"
+
 export TESTDIR="$(cd $(dirname $0) && pwd)"
 export SRCDIR="$(cd $(dirname $0) && cd .. && pwd)"
 export TEXINPUTS="${SRCDIR}::"
 
+# Determine document and driver name
 testscriptbase=$(basename -s .sh "$0")
 for possdriver in pdflatex xelatex lualatex; do
     if echo $testscriptbase | grep -q -e "-${possdriver}$"; then
@@ -14,6 +39,7 @@ for possdriver in pdflatex xelatex lualatex; do
     fi
 done
 
+# per-driver compilation options
 case $DRIVER in
 pdflatex)
     export OPTIONS="-interaction=nonstopmode -halt-on-error"
@@ -30,55 +56,51 @@ lualatex)
     ;;
 esac
 
+# Basic feasibility check
 if [ ! -f "$TESTDIR/$DOCNAME.tex" ]; then
     echo "in $0 - Could not find $TESTDIR/$DOCNAME.tex!"
     exit 2
 fi
 
-echo "---- Test file: $DOCNAME.tex  Driver: $DRIVER ----"
 
-scratch=$(mktemp -d -t tmp.testdissertation.XXXXXXXXXX)
-finish() {
-  if [ -f "$TARGETNAME.pdf" ]; then
-    cp "$TARGETNAME.pdf" "${RESULTSDIR}/${suite}.pdf"
-  fi
-  rm -rf "$scratch"
-}
-trap finish EXIT
+# Clean before build
 
-
-. $TESTDIR/sh2ju.sh
-
-export DIR="$scratch"
-export BASICLOG="$scratch/basiclog.txt"
-export LOG="$scratch/$DOCNAME.log"
-export TARGETNAME="$scratch/$DOCNAME"
-export RESULTSDIR="${juDIR}"
-
-# Running a test means the merged results are invalid - delete them.
-rm -f "${RESULTSDIR}/TESTS-TestSuites.xml"
+rm -f "${RESULTSDIR}/TESTS-TestSuites.xml" # Running a test means the merged results are invalid - delete them.
 # Same thing with the output PDF
-rm -f "${RESULTSDIR}/${suite}.pdf"
-# and for goodness sake, get rid of auxiliary files in the source directory.
-# failing to do so causes weird unexpected errors.
+rm -f "${RESULTSDIR}/${suite}.pdf" # We'll replace this PDF, ditch the old one
+
+# Get rid of auxiliary files in the source directory.
+# Failing to do so causes weird unexpected errors.
 for ext in aux log out toc bbl; do
     rm -f "${TESTDIR}/${DOCNAME}.${ext}"
 done
 
+# Info message before build
+echo "---- Test file: $DOCNAME.tex  Driver: $DRIVER ----"
+
+# END SETUP CODE
+###
+
+# Internal function - not for test script use
+# Perform the actual build of the document
 _runbuild() {
     (
         cd "$scratch"
-
-        # $OPTIONS is if we want to pass in things to use xelatex or something
 
         latexmk -norc -cd- -pdf -g $OPTIONS "$TESTDIR/$DOCNAME" 2>&1 | tee basiclog.txt
     )
 }
 
+# Internal function - not for test script use
+# A wrapper for grep that inverts its exit code: error if it finds something, success if it doesn't
 _oppositegrep() {
     ! grep "$@" 2>&1 >/dev/null
 }
 
+###
+# Build commands and assertions - one of these should be the first assertion in a test script
+# These take no arguments. In addition to the explicit assertion, they also append the contents
+# of the .log file as system output for a second, dummy "test"
 should_build() {
     juLog -name="document should build - console log" -error="Latexmk: Errors, so I did not complete making targets" -error="Fatal error occurred" "_runbuild"
     juLog -name="Full log file" cat "$LOG"
@@ -88,7 +110,11 @@ should_fail_to_build() {
     juLog -name="document should fail to build - console log" -error="Latexmk: All targets.*are up-to-date" "_runbuild"
     juLog -name="Full log file" cat "$LOG"
 }
+###
 
+###
+# Assertions about information printed to the console during build.
+# They take two arguments: the pattern to grep for, and a test name/description
 pass_if_console_matches() {
     juLog -name="$2" "grep \"$1\" \"$BASICLOG\""
 }
@@ -96,7 +122,11 @@ pass_if_console_matches() {
 fail_if_console_matches() {
     juLog -name="$2" "_oppositegrep \"$1\" \"$BASICLOG\""
 }
+###
 
+###
+# Assertions about information written to the .log file during build.
+# They take two arguments: the pattern to grep for, and a test name/description
 pass_if_log_matches() {
     juLog -name="$2" "grep \"$1\" \"$LOG\""
 }
@@ -104,7 +134,11 @@ pass_if_log_matches() {
 fail_if_log_matches() {
     juLog -name="$2" "_oppositegrep \"$1\" \"$LOG\""
 }
+###
 
+###
+# Assertions about the output of arbitrary commands
+# They take three arguments: a (quoted) command to execute, the pattern to grep for, and a test name/description
 pass_if_command_matches() {
     juLog -name="$3" "$1 | grep \"$2\""
 }
@@ -112,10 +146,16 @@ pass_if_command_matches() {
 fail_if_command_matches() {
     juLog -name="$3" "$1 | _oppositegrep \"$2\""
 }
+###
 
+###
+# "Assertion" that simply records a successful test and includes command output
+# in the testing log.
+# It takes two arguments: a (quoted) command to execute, and a name/description
 log_command_output() {
     juLog -name="Output only: $2" "$1"
 }
+###
 
 
 
