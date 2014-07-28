6ccd6c1b77909f6dafb4289fe0fa4f1b1232edd3
Ryan Pavlik <rpavlik@iastate.edu>
2014-07-28 15:00:22 -0500

    use text_of_page in appendices test.

--- tests/test-appendices-pdflatex.sh
+++ tests/test-appendices-pdflatex.sh
@@ -5,9 +5,9 @@
 should_build
 
 # Log the PDFTOTEXT output
-log_command_output "pdftotext -f 1 -l 1 -nopgbrk -layout ${TARGETNAME}.pdf -" "pdftotext page 1"
-log_command_output "pdftotext -f 2 -l 2 -nopgbrk -layout ${TARGETNAME}.pdf -" "pdftotext page 2"
-log_command_output "pdftotext -f 3 -l 3 -nopgbrk -layout ${TARGETNAME}.pdf -" "pdftotext page 3"
+log_command_output "text_of_page 1" "pdftotext page 1"
+log_command_output "text_of_page 2" "pdftotext page 2"
+log_command_output "text_of_page 3" "pdftotext page 3"
 
 pass_if_console_matches "ISU Thesis" "correct document class"
 
@@ -15,12 +15,12 @@ fail_if_console_matches "Package nag Warning" "Looking for complains by nag"
 pass_if_console_matches "No complaints by nag." "Nag package prints 'no complaints' message"
 
 # Check ToC
-pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A.*1" "ToC Multiple appendices: first is A."
-pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*B.*2" "ToC Multiple appendices: second is B."
-fail_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*MY" "ToC Multiple appendices: none should have a missing number."
+pass_if_command_matches "text_of_page 1 -" "APPENDIX[ ]*A.*1" "ToC Multiple appendices: first is A."
+pass_if_command_matches "text_of_page 1" "APPENDIX[ ]*B.*2" "ToC Multiple appendices: second is B."
+fail_if_command_matches "text_of_page 1" "APPENDIX[ ]*MY" "ToC Multiple appendices: none should have a missing number."
 
 # Check chapter titles
-pass_if_command_matches "pdftotext -f 2 -l 2 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A[.]" "Chapter heading multiple appendices: first is A."
-pass_if_command_matches "pdftotext -f 3 -l 3 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*B[.]" "Chapter heading multiple appendices: second is B."
+pass_if_command_matches "text_of_page 2" "APPENDIX[ ]*A[.]" "Chapter heading multiple appendices: first is A."
+pass_if_command_matches "text_of_page 3" "APPENDIX[ ]*B[.]" "Chapter heading multiple appendices: second is B."
 
-fail_if_command_matches "pdftotext -f 2 -l 3 -layout ${TARGETNAME}.pdf -" "APPENDIX[.][ ]*MY" "Chapter heading multiple appendices: no unnumbered appendices!"
+fail_if_command_matches "text_of_page 2 3" "APPENDIX[.][ ]*MY" "Chapter heading multiple appendices: no unnumbered appendices!"
