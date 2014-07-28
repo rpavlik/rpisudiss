26101456480804789f53f13187519959ac79da75
Kevin M. Godby <kevin@godby.org>
2014-07-28 15:24:46 -0500

    Added some more appendix tests.

--- tests/test-appendices-pdflatex.sh
+++ tests/test-appendices-pdflatex.sh
@@ -15,12 +15,17 @@ fail_if_console_matches "Package nag Warning" "Looking for complains by nag"
 pass_if_console_matches "No complaints by nag." "Nag package prints 'no complaints' message"
 
 # Check ToC
-pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A.*1" "ToC Multiple appendices: first is A."
-pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*B.*2" "ToC Multiple appendices: second is B."
-fail_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*MY" "ToC Multiple appendices: none should have a missing number."
+pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+A" "ToC Multiple appendices: first is A."
+pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+B" "ToC Multiple appendices: second is B."
+fail_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+MY" "ToC Multiple appendices: none should have a missing number."
 
-# Check chapter titles
-pass_if_command_matches "pdftotext -f 2 -l 2 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A[.]" "Chapter heading multiple appendices: first is A."
-pass_if_command_matches "pdftotext -f 3 -l 3 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*B[.]" "Chapter heading multiple appendices: second is B."
+# Check appendix headings
+pass_if_command_matches "pdftotext -f 3 -l 3 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+A." "Chapter heading multiple appendices: first is A."
+pass_if_command_matches "pdftotext -f 4 -l 4 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+B." "Chapter heading multiple appendices: second is B."
 
-fail_if_command_matches "pdftotext -f 2 -l 3 -layout ${TARGETNAME}.pdf -" "APPENDIX[.][ ]*MY" "Chapter heading multiple appendices: no unnumbered appendices!"
+fail_if_command_matches "pdftotext -f 3 -l 4 -layout ${TARGETNAME}.pdf -" "APPENDIX\.\s\+MY" "Chapter heading multiple appendices: no unnumbered appendices!"
+
+# Check appendix count
+pass_if_log_matches "The number of appendices in the tex file: 2\." "There are two appendices in the .tex file."
+pass_if_log_matches "The number of appendices in the aux file: 2\." "There are two appendices in the .aux file."
+
--- tests/test-appendix-pdflatex.sh
+++ tests/test-appendix-pdflatex.sh
@@ -11,9 +11,14 @@ fail_if_console_matches "Package nag Warning" "Looking for complains by nag"
 pass_if_console_matches "No complaints by nag." "Nag package prints 'no complaints' message"
 
 # Check ToC
-fail_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A" "Single appendix shouldn't have a number in ToC."
-pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*MY APPENDIX" "Single appendix is unnumbered in ToC."
+fail_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+A" "Single appendix shouldn't have a number in ToC."
+pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+MY APPENDIX" "Single appendix is unnumbered in ToC."
 
-# Check chapter title
-fail_if_command_matches "pdftotext -f 2 -l 2 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A" "Single appendix shouldn't have a number in chapter heading."
-pass_if_command_matches "pdftotext -f 2 -l 2 -layout ${TARGETNAME}.pdf -" "APPENDIX[.][ ]*MY APPENDIX" "Single appendix is unnumbered in chapter heading"
+# Check appendix heading
+fail_if_command_matches "pdftotext -f 4 -l 4 -layout ${TARGETNAME}.pdf -" "APPENDIX\s\+A" "Single appendix shouldn't have a number in chapter heading."
+pass_if_command_matches "pdftotext -f 4 -l 4 -layout ${TARGETNAME}.pdf -" "APPENDIX.\s\+MY APPENDIX" "Single appendix is unnumbered in chapter heading"
+
+# Check appendix count
+pass_if_log_matches "The number of appendices in the tex file: 1\." "There is only one appendix in the .tex file."
+pass_if_log_matches "The number of appendices in the aux file: 1\." "There is only one appendix in the .aux file."
+
