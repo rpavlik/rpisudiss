a0c2a8325fec38e3b6774f5a9aadea4ccffa531a
Ryan Pavlik <rpavlik@iastate.edu>
2014-07-28 14:17:56 -0500

    Try a function to reduce duplication.

--- tests/test-appendix-pdflatex.sh
+++ tests/test-appendix-pdflatex.sh
@@ -10,10 +10,18 @@ pass_if_console_matches "ISU Thesis" "correct document class"
 fail_if_console_matches "Package nag Warning" "Looking for complains by nag"
 pass_if_console_matches "No complaints by nag." "Nag package prints 'no complaints' message"
 
+text_of_page() {
+    if [ -f "${TARGETNAME}.pdf" ]; then
+        pdftotext -f $1 -l $1 -layout -nopgbrk "${TARGETNAME}.pdf" -
+    else
+        echo "Missing output PDF '${TARGETNAME}.pdf'!" >2
+    fi
+}
+
 # Check ToC
-fail_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A" "Single appendix shouldn't have a number in ToC."
-pass_if_command_matches "pdftotext -f 1 -l 1 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*MY APPENDIX" "Single appendix is unnumbered in ToC."
+fail_if_command_matches "text_of_page 1" "APPENDIX[ ]*A" "Single appendix shouldn't have a number in ToC."
+pass_if_command_matches "text_of_page 1" "APPENDIX[ ]*MY APPENDIX" "Single appendix is unnumbered in ToC."
 
 # Check chapter title
-fail_if_command_matches "pdftotext -f 2 -l 2 -layout ${TARGETNAME}.pdf -" "APPENDIX[ ]*A" "Single appendix shouldn't have a number in chapter heading."
-pass_if_command_matches "pdftotext -f 2 -l 2 -layout ${TARGETNAME}.pdf -" "APPENDIX[.][ ]*MY APPENDIX" "Single appendix is unnumbered in chapter heading"
+fail_if_command_matches "text_of_page 2" "APPENDIX[ ]*A" "Single appendix shouldn't have a number in chapter heading."
+pass_if_command_matches "text_of_page 2" "APPENDIX[.][ ]*MY APPENDIX" "Single appendix is unnumbered in chapter heading"
