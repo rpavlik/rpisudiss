59b10fccc666453840eebb7b362badf02a85ba73
Ryan Pavlik <rpavlik@iastate.edu>
2014-07-28 19:45:24 -0500

    Enhance the abstract environment so that it works in both front matter (as a chapter) and main matter (as a section*).
    Fixes #23.
    
    Includes tests in both a full dissertation and a single chapter.

--- tests/test-journalpaperstyle-pdflatex.sh
+++ tests/test-journalpaperstyle-pdflatex.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
+
 . ./testutils.sh
 
 should_build
 
 pass_if_console_matches "ISU Thesis" "correct document class"
 
-fail_if_console_matches "detected driver: pdftex" "this isn't pdftex"
-
-pass_if_log_matches "Generating title page" "full document, should have title page"
-
 fail_if_console_matches "Package nag Warning" "Looking for complains by nag"
 pass_if_console_matches "No complaints by nag." "Nag package prints 'no complaints' message"
 
-
 # Checking behavior of abstract environment in front matter
 pass_if_command_matches "text_of_page 2" "ABSTRACT.*v" "Should have a front-matter abstract listed in TOC"
-pass_if_command_matches "text_of_page 5" "ABSTRACT" "Front-matter abstract where promised, titled in all caps."
+pass_if_command_matches "text_of_page 5" "ABSTRACT" "Front-matter where promised, titled in all caps."
+
+# Checking behavior of abstract environment in a chapter (main matter)
+fail_if_command_matches "text_of_page 2" "Abstract.*1" "Should not see any main matter abstract listed in TOC"
+pass_if_command_matches "text_of_page 8" "Abstract" "Main-matter abstract should be here and not all caps (since it's just a section)"
--- tests/test-single-chapter-journalpaper-pdflatex.sh
+++ tests/test-single-chapter-journalpaper-pdflatex.sh
@@ -9,11 +9,10 @@ pass_if_console_matches "ISU Thesis" "correct document class"
 
 pass_if_console_matches "detected driver: pdftex" "expected pdftex driver in geometry"
 
-pass_if_log_matches "Generating title page" "full document, should have title page"
+fail_if_log_matches "Generating title page" "no title page for single chapter"
 
 fail_if_console_matches "Package nag Warning" "Looking for complains by nag"
 pass_if_console_matches "No complaints by nag." "Nag package prints 'no complaints' message"
 
-# Checking behavior of abstract environment in front matter
-pass_if_command_matches "text_of_page 2" "ABSTRACT.*v" "Should have a front-matter abstract listed in TOC"
-pass_if_command_matches "text_of_page 5" "ABSTRACT" "Front-matter abstract where promised, titled in all caps."
+# Checking behavior of abstract environment in a chapter (main matter)
+pass_if_command_matches "text_of_page 1" "Abstract" "Main-matter abstract should be here and not all caps (since it's just a section)"
