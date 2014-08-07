47b8098ed5b178aad02eabdb8a9ed7923ff04a86
Ryan Pavlik <ryan.pavlik@gmail.com>
2014-08-07 13:44:16 -0500

    Support for alternate xslt2 translator command (mac os x)

--- tests/analyze-results.sh
+++ tests/analyze-results.sh
@@ -2,7 +2,26 @@
 set -e
 TESTDIR="$(cd $(dirname $0) && pwd)"
 
+
+runtransformsaxonb() {
+	saxonb-xslt -s:results/TESTS-TestSuites.xml -xsl:junit-fail-only-greppable.xsl
+}
+
+runtransformsaxon() {
+	saxon results/TESTS-TestSuites.xml junit-fail-only-greppable.xsl
+}
+
+if which saxonb-xslt > /dev/null; then
+	xform=runtransformsaxonb
+elif which saxon > /dev/null; then
+	xform=runtransformsaxon
+else
+	echo "Can't find saxonb-xslt or saxon!" 1>&2
+	exit 1
+fi
+
+
 (
 cd $TESTDIR
-saxonb-xslt -s:results/TESTS-TestSuites.xml -xsl:junit-fail-only-greppable.xsl | (! grep "Failure")
+eval $xform | (! grep "Failure")
 )
